719ce8cb6f96973d36a4dcaadec02241476c877a
panushri25 <p.anushri25@gmail.com>
2016-11-19 18:24:33 -0600

    model2 features

--- conllfeatures.py
+++ conllfeatures.py
@@ -44,22 +44,70 @@ def getMentionFeats(MentionFile, WordsFile, min_count, size, window):
     file.close()
     return mentionFeats
 
+def Word2VecModel2(File, min_count = 1, size = 200, window = 5):
+    file = open(File, "r")
+    words = []
+    sent = []
+    for line in file:
+        line = line.strip()
+        line = line.replace("_"," ")
+        line = line.split(" ")
+        if line[0] == ".":
+            words.append(sent)
+            sent = []
+        else:
+            for word in line:
+                sent.append(word)
+    file.close()
+
+    if len(sent) != 0:
+        words.append(sent)
+
+    model = Word2Vec(words, size=size, window=window, min_count=min_count)
+    return model
+
+def getMentionFeats2(MentionFile, WordsFile, min_count, size, window):
+
+    model = Word2VecModel2(WordsFile, min_count, size, window)
+
+    file = open(MentionFile, "r")
+    mentionFeats = []
+    flag = 0
+    for line in file:
+        line = line.strip()
+        line = line.split(" ")
+        line = line[0]
+        line = line.replace("_"," ")
+        line = line.split(" ")
+        total = np.zeros(size)
+        flag = 0
+        for word in line:
+            total += model[word]
+        if flag == 1:
+            mentionFeats = np.vstack((mentionFeats,total))
+        else:
+            mentionFeats.append(total)
+            flag = 1
+    file.close()
+    return mentionFeats
+
 def getPairFeats(idx,mentionFeats,size):
     PairwiseFeats = np.zeros((len(mentionFeats), size))
     for pidx in range(0,idx):
-        feat1 = mentionFeats[idx][1]
-        feat2 = mentionFeats[pidx][1]
+        feat1 = mentionFeats[idx]
+        feat2 = mentionFeats[pidx]
         dist = feat1 - feat2
         PairwiseFeats[pidx] = dist
+        print(dist)
     return PairwiseFeats
 
 
-# if __name__ == '__main__':
-#     min_count = 1
-#     size = 200
-#     window = 5
+if __name__ == '__main__':
+    min_count = 1
+    size = 200
+    window = 5
 
-#     mentionFeats = getMentionFeats("mentionsList.txt","wordsList.txt",min_count,size,window)
+    mentionFeats = getMentionFeats2("mentionsList.txt","wordsList.txt",min_count,size,window)
 
-#     for idx in range(len(mentionFeats)):
-#         print getPairFeats(idx,mentionFeats)
+    for idx in range(len(mentionFeats)):
+        print(getPairFeats(idx,mentionFeats,size))
